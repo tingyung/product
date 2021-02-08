2bf7a930fa42d2edb261cd66342a9d45dc6ef2ca
Ting <tina19991130@gmail.com>
2021-02-08 16:08:58 +0800

    add version

--- product.py
+++ product.py
@@ -1,13 +1,20 @@
-#讀取檔案
-products = []
-with open("products.csv", "r", encoding = "utf-8") as f:
-	for line in f:
-		if "商品, 價格" in line:
-			continue # 繼續
-		name, price = line.strip().split(",")
-		products.append([name, price])
-print(products)
+import os 
 
+products = []
+if os.path.isfile('products.csv'):
+	print('yeah! 找到檔案了!')
+else:
+	print('找不到檔案')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+	 	for line in f:
+	 		if '商品,價格' in line:
+	 			continue #繼續
+	 		name, price = line.strip().split(',')
+	 		products.append([name, price])
+	 print(products)
+
+
+#讓使用者輸入
 while True:
 	name = input("請輸入商品名稱:")
 	if name == "q":
